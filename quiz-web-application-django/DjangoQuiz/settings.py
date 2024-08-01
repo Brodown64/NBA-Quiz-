abde456b8829ad1286a16ab707db5e8dd49b9abf
Brodown64 <59394822+Brodown64@users.noreply.github.com>
2024-08-01 17:28:59 -0500

    Update settings.py

--- quiz-web-application-django/DjangoQuiz/settings.py
+++ quiz-web-application-django/DjangoQuiz/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '36jk%az^il&d66&dhy(j*!hu9b!oa2imidr5bwqcpwcuwz3cj8'
+SECRET_KEY = '(key is in code dump 2 if ur wondering btw)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
